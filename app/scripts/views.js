b5ab45c629b7cc9ca55c6f3af09cc744ced1f0e8
pavelsof <pavelsof@gmail.com>
2017-01-05 22:29:21 +0100

    The bit is sligthly better.

--- app/scripts/views.js
+++ app/scripts/views.js
@@ -50,16 +50,27 @@ app.views = (function() {
 	var createDataset = function(elem, dataset) {
 		var glosses = app.data.getGlosses(dataset);
 		
-		render(elem, 'dataset-templ', {});
+		render(elem, 'dataset-templ', {dataset: dataset});
 		
 		var glossViewElem = document.getElementById('gloss-view');
 		var searchField = $('[data-fn=choose-gloss]');
 		
-		searchField.typeahead({}, {
+		searchField.typeahead({minLength: 0, highlight: true}, {
+			name: 'glosses',
 			source: function(query, callback) {
+				if(!query) {
+					callback(glosses);
+					return;
+				}
+				
 				var regex = new RegExp(query, 'i');
-				callback(glosses.filter(function(item) {return regex.test(item); }));
-			}
+				
+				callback(glosses.filter(function(item) {
+					return regex.test(item);
+				}));
+			},
+			async: false,
+			limit: 10
 		});
 		
 		searchField.on('typeahead:select typeahead:autocomplete', function(e, value) {
